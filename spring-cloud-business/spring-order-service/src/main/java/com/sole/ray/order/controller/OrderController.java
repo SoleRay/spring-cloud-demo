c0f4d6fe4057cad982024b925d6a5ff8e5707ef8
soleray <pein_zero@126.com>
2021-02-23 22:46:57 +0800

    init

---
+++ spring-cloud-business/spring-order-service/src/main/java/com/sole/ray/order/controller/OrderController.java
@@ -0,0 +1,22 @@
+package com.sole.ray.order.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+
+    @GetMapping("/api-test")
+    public String apiTest() {
+        return null;
+    }
+}
