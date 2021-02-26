b54d127670bb0d10393f87b486bdf8ff38eeebc8
soleray <pein_zero@126.com>
2021-02-26 11:58:38 +0800

    change

---
+++ spring-cloud-hystrix/src/main/java/com/sole/ray/HystrixApplication.java
@@ -0,0 +1,24 @@
+package com.sole.ray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
+/**
+ * 说明
+ *      此版本，禁用Feign自带的Hystrix。只把Fegin当RestTemplate使用
+ *      熔断器单独用@HystrixCommand来实现
+ * 原因
+ *      单独使用@HystrixCommand，其灵活性非常大。
+ *      可以为每个方法单独设置Hystrix属性，指定异常忽略等。
+ */
+
+@EnableHystrix
+@SpringBootApplication
+public class HystrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixApplication.class, args);
+    }
+
+}
