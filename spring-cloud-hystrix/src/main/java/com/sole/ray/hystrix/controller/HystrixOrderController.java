5207343e8fb61980825a93507cb73a2ad12ab170
soleray <pein_zero@126.com>
2021-02-26 11:44:41 +0800

    change

--- spring-cloud-hystrix/src/main/java/com/sole/ray/hystrix/controller/HystrixOrderController.java
+++ spring-cloud-hystrix/src/main/java/com/sole/ray/hystrix/controller/HystrixOrderController.java
@@ -11,12 +11,10 @@ import com.sole.ray.internal.common.entity.Order;
 import com.sole.ray.internal.common.exception.BusinessException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @ResponseResult
