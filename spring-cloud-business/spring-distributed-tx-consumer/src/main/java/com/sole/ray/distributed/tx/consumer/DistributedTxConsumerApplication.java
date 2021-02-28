00814a4ba3040b133722c91deeb68553ec0a9bb2
soleray <pein_zero@126.com>
2021-02-28 22:15:25 +0800

    change

---
+++ spring-cloud-business/spring-distributed-tx-consumer/src/main/java/com/sole/ray/distributed/tx/consumer/DistributedTxConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.sole.ray.distributed.tx.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedTxConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedTxConsumerApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-business/spring-distributed-tx-provider/src/main/java/com/sole/ray/distributed/tx/provider/DistributedTxProviderApplication.java
@@ -0,0 +1,13 @@
+package com.sole.ray.distributed.tx.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedTxProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedTxProviderApplication.class, args);
+    }
+
+}
