59d10e475853b17902b68674d23a175d9db9154a
soleray <pein_zero@126.com>
2021-03-01 16:56:48 +0800

    change

--- spring-cloud-business/spring-distributed-tx-provider/src/main/java/com/sole/ray/distributed/tx/provider/TxProviderApplication.java
+++ spring-cloud-business/spring-distributed-tx-provider/src/main/java/com/sole/ray/distributed/tx/provider/TxProviderApplication.java
@@ -1,11 +1,11 @@
-package com.sole.ray;
+package com.sole.ray.distributed.tx.provider;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan(basePackages = "com.sole.ray")
-@SpringBootApplication
+@MapperScan(basePackages = "com.sole.ray.distributed.tx.provider.dao")
+@SpringBootApplication(scanBasePackages = {"com.sole.ray.distributed.tx.provider","com.sole.ray.internal.common"})
 public class TxProviderApplication {
 
     public static void main(String[] args) {
