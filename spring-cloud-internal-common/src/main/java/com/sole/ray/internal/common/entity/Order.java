75c4ee226a93f3544a44dcc8bfccf7c8a04555d8
soleray <pein_zero@126.com>
2021-03-06 11:25:23 +0800

    change

---
+++ spring-cloud-internal-common/src/main/java/com/sole/ray/internal/common/entity/Order.java
@@ -0,0 +1,15 @@
+package com.sole.ray.internal.common.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+
+    private long id;
+
+    private long orderNo;
+}
